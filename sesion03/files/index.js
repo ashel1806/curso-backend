04106b49d9672fe898d43d2875580e6321bbadb0
Ashel Joseph Vasquez <vasquezpalominoashel@gmail.com>
2023-04-16 18:02:10 -0500

    Se modificó la sesión 03

---
+++ sesion03/files/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// Creación y escritura sobre un archivo
+
+// fs.writeFile('example.txt', 'Hola, desde Node.js', (err) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log('El archivo se ha creado correctamente!!')
+//   }
+// })
+
+// Lectura de un archivo
+
+// fs.readFile('example.txt', function (err, data) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log(data.toString());
+//   }
+// });
+
+// Eliminación de un archivo
+
+// fs.unlink('example.txt', function (err) {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log("El archivo fue eliminado correctamente.");
+//   }
+// });
